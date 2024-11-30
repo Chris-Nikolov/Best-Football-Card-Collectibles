41e3a795b9ed35146f143fb6b744a86e68d2a396
Chris-Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2024-11-30 17:23:35 +0200

    Admin added to accounts

--- djangoBestFootballCardCollectibles/djangoBestFootballCardCollectibles/accounts/admin.py
+++ djangoBestFootballCardCollectibles/djangoBestFootballCardCollectibles/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+
+from djangoBestFootballCardCollectibles.accounts.forms import BFCCUserChangeForm, BFCCUserCreationForm
 
 # Register your models here.
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class BFCCUserAdmin(UserAdmin):
+    form = BFCCUserChangeForm
+    add_form = BFCCUserCreationForm
+    model = UserModel
+    list_display = ('username', 'email', 'is_staff', 'is_active',)
+    search_fields = ('username', 'email')
+    ordering = ('username', 'email',)
+    fieldsets = (('Account', {'fields': ('username', 'email', 'password')}),
+                 ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),)
+    add_fieldsets = (
+        ('Acc', {
+            "classes": ('wide', 'collapse'),
+            "fields": ('username', 'email', 'password1', 'password2'),
+        }),
+    )
