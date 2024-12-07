58619298d963ef62072da31027d531be6b75c19c
Chris-Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2024-12-07 16:17:16 +0200

    Search Update + "See more" page

--- djangoBestFootballCardCollectibles/djangoBestFootballCardCollectibles/web/urls.py
+++ djangoBestFootballCardCollectibles/djangoBestFootballCardCollectibles/web/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from djangoBestFootballCardCollectibles.web.views import index, StaffPageView
+from djangoBestFootballCardCollectibles.web.views import index, StaffPageView, search, AllCardsView
 
 urlpatterns = [
     path('', index, name='index'),
-    path('staff-page/', StaffPageView.as_view(), name='staff_page')
+    path('staff-page/', StaffPageView.as_view(), name='staff_page'),
+    path('search-results/', search, name='search'),
+    path('all-cards/', AllCardsView.as_view(), name='all_cards'),
 ]
--- djangoBestFootballCardCollectibles/djangoBestFootballCardCollectibles/web/views.py
+++ djangoBestFootballCardCollectibles/djangoBestFootballCardCollectibles/web/views.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from django.views.generic import ListView
 
 from djangoBestFootballCardCollectibles.cards.models import Card
+from djangoBestFootballCardCollectibles.web.forms import SearchForm
 
 
 # Create your views here.
 
 
 def index(request):
-    cards = Card.objects.filter(is_approved=True).order_by('-created_at')
+    cards = Card.objects.filter(is_approved=True).order_by('-created_at')[:6]
     context = {"cards": cards}
     return render(request, 'web/index.html', context)
 
@@ -25,3 +27,36 @@ class StaffPageView(UserPassesTestMixin, ListView):
 
     def get_queryset(self):
         return Card.objects.filter(is_approved=False, is_for_sale=True).order_by('-created_at')
+
+
+def search(request):
+    form = SearchForm()
+    results = []
+    query = ''
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Card.objects.filter(title__icontains=query, is_approved=True).order_by('-created_at')
+
+        paginator = Paginator(results, 5)
+        page = request.GET.get('page')
+        try:
+            results = paginator.page(page)
+        except PageNotAnInteger:
+            results = paginator.page(1)
+        except EmptyPage:
+            results = paginator.page(paginator.num_pages)
+
+    context = {'form': form, 'results': results, 'query': query}
+    return render(request, 'web/search-page.html', context)
+
+
+class AllCardsView(ListView):
+    model = Card
+    template_name = 'web/all-cards-page.html'
+    context_object_name = 'cards'
+    paginate_by = 6
+
+    def get_queryset(self):
+        return Card.objects.filter(is_approved=True).order_by('-created_at')
